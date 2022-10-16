48d00d005f4d6fcbed802d18c5caafe1475ebd14
ayuubb <ayubbkj2@gmail.com>
2022-10-16 17:00:32 +0700

    add service

---
+++ api/pelaksanaan/pelaksanaan.controller.js
@@ -0,0 +1,79 @@
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+const {
+  getAll, getById
+} = require("./pelaksanaan.service");
+
+module.exports = {
+  getAll: (req, res) => {
+    getAll((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getById: (req, res) => {
+    const id = req.params.id;
+    getById(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.status(404).json({
+          success: 0,
+          message: "Record not Found",
+        });
+      }
+
+      results.password = undefined;
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  // login: (req, res) => {
+  //   const body = req.body;
+  //   getById(body.nim, (err, results) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     if (!results) {
+  //       return res.status(401).json({
+  //         success: 0,
+  //         data: "Invalid nim or password",
+  //       });
+  //     }
+
+  //     // const result = compareSync(body.password, results.password);
+
+  //     if (body.password === results.password) {
+  //       results.password = undefined;
+  //       results.password2 = undefined;
+
+  //       const jsontoken = sign({ result: results }, process.env.JWT_KEY, {
+  //         expiresIn: "1d",
+  //       });
+
+  //       return res.status(200).json({
+  //         success: 1,
+  //         message: "login successfully",
+  //         token: jsontoken,
+  //         uid: results.nim,
+  //       });
+  //     } else {
+  //       return res.status(401).json({
+  //         success: 0,
+  //         data: "Invalid nim or password",
+  //       });
+  //     }
+  //   });
+  // },
+};
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const mahasiswaRoute = require('./api/mahasiswa/mahasiswa.router');
 const portofolioRoute = require('./api/portofolio/portofolio.router');
+const pelaksanaanRoute = require('./api/pelaksanaan/pelaksanaan.router');
+const laporanRoute = require('./api/laporan/laporan.router');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +16,8 @@ app.use(express.json());
 
 app.use('/api/mahasiswa', mahasiswaRoute);
 app.use('/api/portofolio', portofolioRoute);
+app.use('/api/pelaksanaan', pelaksanaanRoute);
+app.use('/api/laporan', laporanRoute);
 
 const port = process.env.APP_PORT;
 app.listen(port, () => {
