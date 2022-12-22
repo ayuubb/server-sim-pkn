87baf6ff5f8f1aed12e6336c8b15bf41a5478183
ayuubb <ayubbkj2@gmail.com>
2022-12-22 11:26:48 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { createPool } = require("mysql");
 
 const pool = createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || '192.168.1.32',
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
