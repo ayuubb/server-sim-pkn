1013dadbe586890c932c733db3efcb4c591fa2ae
ayuubb <ayubbkj2@gmail.com>
2023-01-11 17:18:15 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
-const { createPool } = require("mysql");
+const { createPool } = require('mysql');
 
 const pool = createPool({
-  host: process.env.DB_HOST || '192.168.1.32',
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  // connectionLimit: 10
+    host: process.env.DB_HOST || '172.20.10.5',
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB,
+    // connectionLimit: 10
 });
 
 module.exports = pool;
